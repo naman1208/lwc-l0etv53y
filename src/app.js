2f8d85d1c22c80f99abeb666f0fdaff1b8cfedc9
NAMAN CHAURASIA <37123345+undefined@users.noreply.github.com>
2022-03-06 09:36:03 +0000

    Push from master

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
-<template>
-    <input value={message}></input>
-</template>
+import { LightningElement } from 'lwc';
+export default class App extends LightningElement {
+  message = 'Hello World';
+}
